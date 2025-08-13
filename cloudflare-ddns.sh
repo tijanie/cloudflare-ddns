6ffd8b7154673b2ac46378ec780b95b8c579eb74
tijanie <irfantijanie7@gmail.com>
2025-08-13 14:38:40 +0800

    add log file

--- cloudflare-ddns.sh
+++ cloudflare-ddns.sh
@@ -87,6 +87,8 @@ change_ip(){
 	
 	# keep changed IP in json file
 	ip_change=$(jq '.records.content = "'"$IP"'"' cloudflare-ddns.json)
+	#write to log
+	echo "$(jq .records.content cloudflare-ddns.json | tr -d '"') > ${IP} - $(date)" > cloudflare-ddns.log
 	echo ${ip_change} | jq '.' > cloudflare-ddns.json
 	echo -e "\n"
 }
